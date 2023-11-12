af41dc74af3fe880269b95c947bab535214b58b4
Cristian Lesmes <119712186+lezmez@users.noreply.github.com>
2023-11-12 08:45:06 -0500

    Add files via upload

---
+++ Floristeria_Pétalos_Encantados/index.js
@@ -0,0 +1,10 @@
+// Definición de productos
+const Catálogo = [
+    { nombre: 'Rosa Roja', tipo: 'Rosas', precio: 10.000 },
+    { nombre: 'Rosa Blaca', tipo: 'Rosas', precio: 10.000 },
+    
+];
+
+// Funcionalidades JS
+// (Agrega aquí el código JavaScript para las funcionalidades solicitadas)
+
